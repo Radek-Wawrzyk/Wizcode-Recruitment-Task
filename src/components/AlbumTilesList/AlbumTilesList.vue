fe26a8df2e0525b99ee05939d7572db62cc2464b
Radosław <radekwawrzyk@gmail.com>
2025-03-26 18:41:55 +0100

    App - Refactor over the current views & components

---
+++ src/components/AlbumTilesList/AlbumTilesList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AlbumTile from '@/components/AlbumTile/AlbumTile.vue';
+import type { Album } from '@/types/Album.type';
+
+defineProps<{
+  albums: Album[];
+}>();
+</script>
+
+<template>
+  <ul class="album-tiles-list">
+    <AlbumTile
+      v-for="album in albums"
+      :key="album.id"
+      :image="album.image"
+      :name="album.name"
+      :artist="album.artistName"
+      :release-date="album.releaseDate"
+    />
+  </ul>
+</template>
+
+<style lang="scss" scoped src="./AlbumTilesList.scss" />
--- src/pages/TopAlbumsPage/TopAlbumsPage.vue
+++ src/pages/TopAlbumsPage/TopAlbumsPage.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
-import AlbumTile from '@/components/AlbumTile/AlbumTile.vue';
+import AlbumTilesList from '@/components/AlbumTilesList/AlbumTilesList.vue';
 
 import { useTopAlbums } from '@/composables/useTopAlbums';
 import { useI18n } from 'vue-i18n';
 
 const { t } = useI18n();
-const { topAlbums, isLoading } = useTopAlbums();
+const { topAlbums } = useTopAlbums();
 </script>
 
 <template>
-  <div class="top-albums container">
-    <header class="top-albums-header">
-      <h1 class="top-albums-header__title">{{ t('topAlbums.title') }}</h1>
-    </header>
+  <div class="top-albums page">
+    <div class="container">
+      <header class="top-albums-header">
+        <h1 class="top-albums-header__title">{{ t('topAlbums.title') }}</h1>
+      </header>
 
-    <section class="top-albums-content">
-      <ul v-if="!isLoading && topAlbums" class="top-albums-content__list">
-        <AlbumTile
-          v-for="album in topAlbums"
-          :key="album.id"
-          :image="album.image"
-          :name="album.name"
-          :artist="album.artistName"
-          :release-date="album.releaseDate"
-        />
-      </ul>
-    </section>
+      <section class="top-albums-content">
+        <AlbumTilesList :albums="topAlbums" />
+      </section>
+    </div>
   </div>
 </template>
 
